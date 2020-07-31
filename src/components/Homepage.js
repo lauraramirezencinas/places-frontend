0f8c469288d124dbbcb2b896dc757088eeedf5d0
ltreven <ltreven@gmail.com>
2020-07-31 22:39:35 +0200

    added routes

---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>homepage</h1>
+            </div>
+        )
+    }
+}
+
+export default Homepage
