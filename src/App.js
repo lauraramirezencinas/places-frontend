4019830801b6ef83d730a5cadd31c02245e7a1dc
Alba Olveros <x8a.018@gmail.com>
2020-08-01 12:52:57 +0200

    Admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
+import Admin from './components/Admin'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
+        <Route exact path="/admin" render={(props) => <Admin {...props} />} />
       </Switch>
     </div>
   );
