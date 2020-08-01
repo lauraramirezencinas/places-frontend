950a0b61f0b445d6b2d5a8d0c8095d4f57bb6d2a
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-08-01 12:16:04 +0200

    search

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Search from './components/Search';
+import SearchLocation from './components/SearchLocation';
+
 
 function App() {
+
+
   return (
     <div>
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
+        
       </Switch>
-      <Search />
+      
     </div>
   );
 }
