fa0fef3e3c9ad0ae004394e0a8853612cc606492
Marc De Mena <mdemena@gmail.com>
2020-08-01 19:26:49 +0200

    Las merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {Switch, Route} from "react-router-dom";
-import Homepage from "./components/Homepage";
-import NavigationBar from "./components/NavigationBar";
-import Lista from "./components/List/Lista";
-import Admin from "./components/Admin";
-import AddPlace from "./components/addplace/AddPlace";
-import EditPlace from "./components/EditPlace";
-import PlaceDetails from "./components/PlaceDetails";
-import SearchLocation from "./components/SearchLocation";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import NavigationBar from './components/NavigationBar';
+import Lista from './components/List/Lista';
+import Admin from './components/Admin';
+import AddPlace from './components/addplace/AddPlace';
+import EditPlace from './components/EditPlace';
+import PlaceDetails from './components/PlaceDetails';
+import SearchLocation from './components/SearchLocation';
 
 function App() {
 	return (
@@ -20,16 +20,16 @@ function App() {
 				<Route excat path="/list" component={Lista} />
 				<Route exact path="/add" component={AddPlace} />
 				<Route exact path="/admin" render={(props) => <Admin {...props} />} />
-<<<<<<< HEAD
-				<Route exact path="/search" render={(props) => <SearchLocation {...props} />} />
-				<Route exact path="/place/:id/edit" render={(props) => <EditPlace {...props} />} />
-=======
 				<Route
 					exact
 					path="/search"
 					render={(props) => <SearchLocation {...props} />}
 				/>
->>>>>>> 84a007a79d2a63a176f1f1c436eff5fe42258044
+				<Route
+					exact
+					path="/place/:id/edit"
+					render={(props) => <EditPlace {...props} />}
+				/>
 			</Switch>
 		</div>
 	);
